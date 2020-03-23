34206f8096bcefd5da2e86b48b49755902dddbab
Bmercado17 <askotala@yahoo.com>
2020-03-22 20:39:18 -0700

    second commit

--- myScript.js
+++ myScript.js
@@ -1,1 +1,27 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  var current = 1;
+  var itemLength = $(".card-container .item").length; // I could also just use the number 3 for the 3 slides and it works.
+
+  $(".cardSlider .btn-prev").on("click", function() {
+    if (current > 1) {
+      current = current - 1;
+      showSlider(current);
+    } else {
+      current = itemLength;
+      showSlider(current);
+    }
+  });
+  $(".cardSlider .btn-next").on("click", function() {
+    if (current !== itemLength) {
+      current = current + 1;
+      showSlider(current);
+    } else {
+      current = 1;
+      showSlider(current);
+    }
+  });
+  function showSlider(current) {
+    $(".card-container .item").removeClass("active");
+    $('div[data-id="' + current + '" ]').addClass("active");
+  }
+});
